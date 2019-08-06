fa7934b34b8288e9a831762724b2aac2bd6c9c58
marcel <marcel.1710@live.de>
2019-08-06 12:53:24 +0200

    Initial python file

--- Exercise1.py
+++ Exercise1.py
@@ -14,6 +14,7 @@ min_interval = -4
 max_interval = 4
 
 class Bandit():
+    rewards_obtained = []
     def __init__(self, mean):
         self.mean = mean
     def pull(self):
@@ -41,16 +42,25 @@ def epsilon_greedy():
         chosen_bandit = bandit_list[index]
         reward = chosen_bandit.pull()
         total_reward += reward
+        chosen_bandit.rewards_obtained.append(reward)
         avg_reward_list.append(total_reward/(i+1))
         N[index] += 1
         Q[index] = Q[index] + (1 / N[index]) * (reward - Q[index])
 
-    plt.plot([x for x in range(arm_pulls)], ucb(), 'ro', label='Average reward')
+    #plt.plot(0, bandit_list[0].rewards_obtained, 'go', label='Reward distribution')
+    #plt.plot([x for x in range(arm_pulls)], avg_reward_list, 'go', label='Epsilon Greedy')
+    #plt.title('The best plot ever with epsilon =%f' % epsilon)
+    #plt.xlabel('Arm pulls')
+    #plt.ylabel('Average reward')
+
+    #for xe, ye in zip([1], [tuple(bandit_list[0].rewards_obtained)]):
+    #    plt.scatter([xe] * len(ye), ye)
+    plt.hist2d(bandit_list[0].rewards_obtained, bins=(1,len(bandit_list[0].rewards_obtained)))
     plt.title('The best plot ever with epsilon =%f' % epsilon)
-    plt.xlabel('Arm pulls')
-    plt.ylabel('Average reward')
+    plt.xlabel('Bandits')
+    plt.ylabel('Reward distribution')
     plt.legend()
-    plt.show()
+
 
 
 #Upper Confidence Bound (UCB)
@@ -77,16 +87,50 @@ def ucb():
         total_reward += reward
         avg_reward_list.append(total_reward / (t+1))
         N[index] += 1
-    plt.plot([x for x in range(arm_pulls)], avg_reward_list, 'ro', label='Average reward')
+        Q[index] = Q[index] + (1 / N[index]) * (reward - Q[index])
+    plt.plot([x for x in range(arm_pulls)], avg_reward_list, 'bo', label='UCB')
     plt.title('Ubc with c=%f'  %c)
     plt.xlabel('Arm pulls')
     plt.ylabel('Average reward')
     plt.legend()
-    plt.show()
+
+#Greedy with optimistic values
+def optimistic_greedy():
+    bandit_list = []
+    for i in range(10):
+        bandit_list.append(Bandit(random.randint(min_interval, max_interval)))
+    total_reward = 0
+    epsilon = 0
+    optimistic_init_value = 5
+    Q = [optimistic_init_value for x in range(bandit_amount)]
+    N = [0 for x in range(bandit_amount)]
+    avg_reward_list = []
+    for i in range(arm_pulls):
+        action = random.random() #random.uniform(0,1)
+        if action < epsilon:                        #With prob. epsilon: explore
+            index = random.randint(0,bandit_amount-1)
+        else:
+            index = np.argmax(Q)
+        chosen_bandit = bandit_list[index]
+        reward = chosen_bandit.pull()
+        total_reward += reward
+        avg_reward_list.append(total_reward/(i+1))
+        N[index] += 1
+        Q[index] = Q[index] + (1 / N[index]) * (reward - Q[index])
+
+    plt.plot([x for x in range(arm_pulls)], avg_reward_list, 'ro', label='Optimistic Greedy')
+    plt.title('Optimistic initial value =%f' %optimistic_init_value)
+    plt.xlabel('Arm pulls')
+    plt.ylabel('Average reward')
+    plt.legend()
 
 
 
-ucb()
+#ucb()
+epsilon_greedy()
+#optimistic_greedy()
+
+plt.show()
 #’bo’ is for blue dot, ‘b’ is for solid blue line
 #plt.plot([x for x in range(10)], Q, 'bo', label='Expected reward')
 #plt.plot([x for x in range(10)], N, 'yo', label='Number of arm pulls')
